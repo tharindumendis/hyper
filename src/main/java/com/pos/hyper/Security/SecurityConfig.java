ff9773a5ea949472803c3f2ab6a045f1ed32e519
Tharindu Dahampriya Mendis <tharindumendis100@outlook.com>
2025-05-16 02:34:13 +0530

    test: port change

--- src/main/java/com/pos/hyper/Security/SecurityConfig.java
+++ src/main/java/com/pos/hyper/Security/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
       
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://hyperpospack.netlify.app","http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://hyperpospack.netlify.app","http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
