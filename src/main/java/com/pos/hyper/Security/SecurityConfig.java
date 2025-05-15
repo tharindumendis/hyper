60ea9dda87e0ce6c6d1815ce6953310009649826
Tharindu Dahampriya Mendis <tharindumendis100@outlook.com>
2025-05-16 01:58:37 +0530

    fix: config front end

--- src/main/java/com/pos/hyper/Security/SecurityConfig.java
+++ src/main/java/com/pos/hyper/Security/SecurityConfig.java
@@ -58,19 +58,12 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
       
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://hyperpospack.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-
-        CorsConfiguration configurationnetlyfy = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://hyperpospack.netlify.app"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-        source.registerCorsConfiguration("/**", configurationnetlyfy);
 
         CorsConfiguration openConfig = new CorsConfiguration();
         openConfig.setAllowedOrigins(List.of("*"));
