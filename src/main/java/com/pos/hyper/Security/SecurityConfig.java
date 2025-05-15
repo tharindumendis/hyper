c2ecd4f40267add398057d67ad746184d5ec964a
Tharindu Dahampriya Mendis <tharindumendis100@outlook.com>
2025-05-16 02:20:29 +0530

    fix: localhost:3000 add for testing

--- src/main/java/com/pos/hyper/Security/SecurityConfig.java
+++ src/main/java/com/pos/hyper/Security/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
       
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://hyperpospack.netlify.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://hyperpospack.netlify.app","http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
