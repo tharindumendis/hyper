edcde1261e902f7e6129a2cb1880609d57c7eaca
Tharindu Dahampriya Mendis <tharindumendis100@outlook.com>
2025-05-18 01:36:07 +0530

    fix:import error

--- src/main/java/com/pos/hyper/controller/ProductController.java
+++ src/main/java/com/pos/hyper/controller/ProductController.java
@@ -8,6 +8,7 @@ import com.pos.hyper.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
